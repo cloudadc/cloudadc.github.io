37a26db2f58ed76b0f63aa9a2e4e41452c11efb4
Kylin Soong <kylinsoong.1214@gmail.com>
2019-11-12 15:28:04 +0800

    Deploy document

--- content/etc/pov/demo_settings.py
+++
@@ -1,4 +0,0 @@
-URI_STRING ="mongodb://root:mongo@ksoong.local:27000,ksoong.local:27001,ksoong.local:27002/admin?replicaSet=repl-1&authSource=admin"
-#URI_STRING = "mongodb+srv://main_user:mypasswordtestcluster-abcd.mongodb.net/test?retryWrites=true"
-NUM_ITEMS = 10000
-NUM_SAMPLING = 1000
